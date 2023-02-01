86d2b4a078079d2d8083785e8fc17dfb7adf670c
Tryzack <44532224+Tryzack@users.noreply.github.com>
2023-02-01 17:55:09 -0400

    Update asteroides.cpp

--- asteroides.cpp
+++ asteroides.cpp
@@ -1,3 +1,6 @@
+//COMENTARIO GENERAL: la funcion de tiro tiene problemas actualmente, es recomendable no utilizar
+//Se reparo el problema de que no podia ir de izquierda a derecha, las balas al tocar el infinito empiezan a llover de manera aleatoria
+
 #include<stdio.h>
 #include<windows.h>
 #include<conio.h>
@@ -10,7 +13,7 @@ using namespace std;
 #define DERECHA 77
 #define ABAJO 80
 
-
+//FUNCION GOTOXY GENERAL
 void gotoxy(int x, int y){
     HANDLE hCon;
     hCon = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -21,6 +24,7 @@ void gotoxy(int x, int y){
 
 }
 
+//FUNCION OCULTAR CURSOR DE LA CONSOLA
 void ocultarcursor(){
     HANDLE hCon;
     hCon = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -31,6 +35,7 @@ void ocultarcursor(){
     SetConsoleCursorInfo(hCon, &cci);
 }
 
+//FUNCION PARA DIBUJAR LIMITES DEL MAPA
 void pintarlimites(){
     for(int i=2; i<78; i++){
         gotoxy(i, 3); printf("%c", 205);
@@ -47,10 +52,12 @@ void pintarlimites(){
     gotoxy(77,33); printf("%c", 188);
 }
 
+//CLASE DE LA NAVE
 class nave{
     int x, y;
     int vida, vidas;
     public:
+    //CONSTRUCTOR
     nave(int _x, int _y, int _vida, int _vidas): x(_x), y(_y), vida(_vida), vidas(_vidas){};
 
     void pintar();
@@ -65,24 +72,29 @@ class nave{
 
 };
 
+//FUNCION PARA DIBUJAR LA NAVE
 void nave::pintar(){
+
     gotoxy(x,y); printf("  %c", 30);
     gotoxy(x, y+1); printf(" %c%c%c", 40, 207, 41);
     gotoxy(x, y+2); printf("%c%c %c%c", 30, 219, 219, 30);
+
 }
+//FUNCION PARA BORRAR LA NAVE
 void nave::borrar(){
     gotoxy(x, y); printf("      ");
     gotoxy(x, y+1); printf("      ");
     gotoxy(x, y+2); printf("      ");
 }
+//FUNCION PARA MOVER LA NAVE
 void nave::mover(){
 
         if(kbhit()){
             char tecla = getch();
             gotoxy(x,y);
             borrar();
-            if(tecla == IZQUIERDA && x>3) x=x--;
-            if(tecla == DERECHA && x+6<77) x=x++;
+            if(tecla == IZQUIERDA && x>3) x--;
+            if(tecla == DERECHA && x+6<77) x++;
             if(tecla == ARRIBA &&y>4) y--;
             if(tecla == ABAJO &&y+3<33) y++;
             pintar();
@@ -90,6 +102,7 @@ void nave::mover(){
         }
 
 }
+//FUNCION PARA DIBUJAR LAS VIDAS RESTANTES
 void nave::pintar_vida(){
 
     gotoxy(50,2); printf("VIDAS: %d", vidas);
@@ -99,6 +112,7 @@ void nave::pintar_vida(){
         gotoxy(70+i,2); printf("%c", 3);
     }
 }
+//FUNCION DE MUERTE DE LA NAVE
 void nave::muerte(){
     if(vida==0){
         borrar();
@@ -123,26 +137,31 @@ void nave::muerte(){
 
 
 }
+//FUNCION PARA DEVOLVER VIDAS RESTANTES
 int nave::preguntar_vida(){
     return vidas;
 }
+//FUNCION PARA DISMINUIR VIDA
 void nave::disminuir_vida(){
 vida--;
 }
 
+//CLASE DE LOS ASTEROIDES
 class asteroide{
     int x, y;
 public:
+    //CONSTRUCTOR DEL ASTEROIDE
     asteroide(int _x, int _y): x(_x), y(_y){}
     void pintar();
     void mover();
     void colision(nave &n);
 
 };
-
+//FUNCION PARA DIBUJAR EL ASTEROIDE
 void asteroide::pintar(){
     gotoxy(x,y); printf("x");
 }
+//FUNCION PARA MOVER AL ASTEROIDE
 void asteroide::mover(){
     gotoxy(x,y); printf(" ");
     y++;
@@ -152,6 +171,7 @@ void asteroide::mover(){
     }
     pintar();
 }
+//FUNCION PARA LA COLISION ENTRE ASTEROIDE Y NAVE
 void asteroide::colision(nave &n){
 
     if(x >= n.X() && x <= n.X()+5 && y>=n.Y() && y<=n.Y()+2){
@@ -167,38 +187,44 @@ void asteroide::colision(nave &n){
 
 }
 
+//CLASE DE TIROS O BALAS
 class tiro{
     int x, y;
 public:
+    //CONSTRUCTOR DE LOS ASTEROIDES
     tiro(int _x, int _y): x(_x), y(_y){}
     void mover();
     bool fuera() {if(y==4) return true; return false;}
     int X(){return x;}
     int Y(){return y;}
 };
-
+//FUNCION PARA MOVER LOS TIROS
 void tiro::mover(){
     gotoxy(x,y); printf(" ");
     y--;
     gotoxy(x,y); printf("*");
 }
 
+
+//MAIN
 int main (){
 
+    //DIBUJAR LIMITES DEL MAPA, NAVE Y CREACION DE ASTEROIDES
     pintarlimites();
     nave n(38,25, 3, 3);
     n.pintar();
     n.pintar_vida();
-
     asteroide a1(10, 4), a2(10, 8), a3(10, 12), a4(10, 16), a5(10, 20), a6(10, 24);
+    bool fin = false;
 
+    //CREACION DE LAS LISTAS DINAMICAS PARA LOS TIROS
     list<tiro*> b;
     list<tiro*>::iterator it;
 
-    bool fin = false;
-
+    
     while(!fin){
 
+        //CREACION DE LOS TIROS (falta crear una funcion para esto)
         if(kbhit()){
             char tecla = getch();
             if (tecla == 'a')
@@ -207,23 +233,28 @@ int main (){
 
         for(it = b.begin(); it != b.end(); it++){
 
+
             (*it) -> mover();
-            if((*it) -> fuera){
+
+            //ERROR EN EL SIGUIENTE CODIGO COMENTADO
+
+            /*if((*it) -> fuera){
                 gotoxy((*it)->X(),(*it)->Y()); printf(" ");
                 delete(*it);
                 it = b.erase(it);
-            }
-        }
 
+            }*/
+       }
 
         ocultarcursor();
+        //CREACION DE LOS ATEROIDES EXISTENTES
         a1.mover(); a1.colision(n);
         a2.mover(); a2.colision(n);
         a3.mover(); a3.colision(n);
         a4.mover(); a4.colision(n);
         a5.mover(); a5.colision(n);
         a6.mover(); a6.colision(n);
-
+        //CREACION DE LA NAVE
         n.muerte();
         n.mover();
 
@@ -240,7 +271,7 @@ int main (){
             Sleep(5000);
             fin=true;
         }
-        }
+    }
 
     return 0;
 }
